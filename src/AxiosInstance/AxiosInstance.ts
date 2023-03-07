f7f7144ea959fe04c5a5305291cb51eb7946bf7d
Saman <saman@foodbomb.com.au>
2023-03-07 16:06:28 +0800

    add correct url

--- src/AxiosInstance/AxiosInstance.ts
+++ src/AxiosInstance/AxiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Environment } from '../utils/Enums&Constants';
 
-const serverBaseURL = Environment.DEVELOPMENT ? 'https://image-upload-msc8.onrender.com/' : 'https://something.com';
+const serverBaseURL = Environment.PRODUCTION ? 'https://image-upload-msc8.onrender.com/' : 'http://localhost:8080';
 const axiosInstance = axios.create({
   baseURL: serverBaseURL,
 });
